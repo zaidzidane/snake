34c77dd5c2ed119792f300280fa907a502a36843
Mohd Zaid <diaz.zidane@gmail.com>
2019-01-23 21:05:19 +0530

    Add files via upload

---
+++ perceptron.py
@@ -0,0 +1,32 @@
+import numpy as np 
+import pandas as pd
+
+
+
+x1=np.array([(0,0),(0,1),(1,0),(1,1)])
+
+
+theta=2; 
+#theta=1; 
+
+
+for i in x1:
+      
+
+   t=np.sum(i)     
+   if(t>=theta):
+		
+
+	     print(1)
+
+
+   else: 
+
+	     print(0);         
+
+
+
+    
+
+
+	
